18f072a3a44d892ae77eceb6a82e5f96192b63f9
J Miller <j@jmillerdesign.com>
2013-12-01 00:50:05 -0800

    Add PHP 5.4 STRICT support

--- Model/Behavior/LogActionBehavior.php
+++ Model/Behavior/LogActionBehavior.php
@@ -28,14 +28,14 @@ class LogActionBehavior extends ModelBehavior {
  * - fields: (array) List of fields to monitor. DEFAULTS TO: none,
  * - trackDelete: (boolean) True if row deletions should be monitored. DEFAULTS TO: true
  *
- * @param Model $Model Model using the behavior
- * @param array $settings Settings to override for model.
+ * @param Model $model Model using the behavior
+ * @param array $config Settings to override for model.
  * @access public
  */
-	public function setup(&$Model, $settings = array()) {
-		if (!isset($this->settings[$Model->alias])) {
+	public function setup(Model $model, $config = array()) {
+		if (!isset($this->settings[$model->alias])) {
 			// Set default settings to class variable
-			$this->settings[$Model->alias] = array(
+			$this->settings[$model->alias] = array(
 				'authSession' => 'Auth',
 				'userModel'   => 'User',
 				'fields'      => array(),
@@ -44,25 +44,25 @@ class LogActionBehavior extends ModelBehavior {
 		}
 
 		// Merge default settings with custom settings
-		$this->settings[$Model->alias] = array_merge($this->settings[$Model->alias], $settings);
+		$this->settings[$model->alias] = array_merge($this->settings[$model->alias], $config);
 
-		if ($this->settings[$Model->alias]['trackDelete']) {
+		if ($this->settings[$model->alias]['trackDelete']) {
 			// Track deletions
 
 			// Track SoftDeletable behavior
 			// @link http://github.com/evilbloodydemon/cakephp-softdeletable2
-			if (in_array('SoftDeletable', $Model->actsAs)) {
+			if (in_array('SoftDeletable', $model->actsAs)) {
 				// Make sure 'deleted' field is being monitored
 				// beforeDelete & afterDelete are not called if using SoftDeletable
-				$softDeletableField = (!empty($Model->actsAs['SoftDeletable']['field'])) ? $Model->actsAs['SoftDeletable']['field'] : 'deleted';
-				if ($Model->hasField($softDeletableField) && !in_array($softDeletableField, $this->settings[$Model->alias]['fields'])) {
-					$this->settings[$Model->alias]['fields'][] = $softDeletableField;
+				$softDeletableField = (!empty($model->actsAs['SoftDeletable']['field'])) ? $model->actsAs['SoftDeletable']['field'] : 'deleted';
+				if ($model->hasField($softDeletableField) && !in_array($softDeletableField, $this->settings[$model->alias]['fields'])) {
+					$this->settings[$model->alias]['fields'][] = $softDeletableField;
 				}
 			}
 		}
 
 		// Set blank default values for changed fields
-		$this->_changes[$Model->alias] = array(
+		$this->_changes[$model->alias] = array(
 			'before'     => array(),
 			'after'      => array(),
 			'hasChanges' => false
@@ -75,58 +75,58 @@ class LogActionBehavior extends ModelBehavior {
  * Monitors data that is about to be saved. Will read existing data from the database,
  * and if changes have occurred, will set them in the 'changes' key
  *
- * @param Model $Model Model instance
+ * @param Model $model Model instance
  * @return boolean true to continue, false to abort the save
  * @access public
  */
-	public function beforeSave(&$Model) {
-		if (!$this->settings[$Model->alias]['fields']) {
+	public function beforeSave(Model $model) {
+		if (!$this->settings[$model->alias]['fields']) {
 			// No fields to monitor
 			return true;
 		}
 
 		// Check if any fields we are monitoring are in $this->data
-		foreach ($this->settings[$Model->alias]['fields'] as $field) {
-			if (in_array($field, array_keys($Model->data[$Model->alias]))) {
-				$this->_changes[$Model->alias]['after'][$field] = $Model->data[$Model->alias][$field];
+		foreach ($this->settings[$model->alias]['fields'] as $field) {
+			if (in_array($field, array_keys($model->data[$model->alias]))) {
+				$this->_changes[$model->alias]['after'][$field] = $model->data[$model->alias][$field];
 			}
 		}
 
-		if (!$this->_changes[$Model->alias]['after']) {
+		if (!$this->_changes[$model->alias]['after']) {
 			// No monitored fields are being updated
 			return true;
 		}
 
 		// Read the existing data before updating
-		$oldData = $Model->find('first', array(
+		$oldData = $model->find('first', array(
 			'conditions' => array(
-				$Model->alias.'.id' => $Model->id
+				$model->alias.'.id' => $model->id
 			),
-			'fields' => array_keys($this->_changes[$Model->alias]['after'])
+			'fields' => array_keys($this->_changes[$model->alias]['after'])
 		));
 
 		if ($oldData) {
-			$this->_changes[$Model->alias]['before'] = $oldData[$Model->alias];
+			$this->_changes[$model->alias]['before'] = $oldData[$model->alias];
 		} else {
 			// Creating a new entry
 			// Make before match after, but with empty values
-			foreach ($this->_changes[$Model->alias]['after'] as $key => $value) {
-				$this->_changes[$Model->alias]['before'][$key] = '';
+			foreach ($this->_changes[$model->alias]['after'] as $key => $value) {
+				$this->_changes[$model->alias]['before'][$key] = '';
 			}
 		}
 
 		// Check if any values have changed
-		foreach ($this->_changes[$Model->alias]['after'] as $field => $newValue) {
-			$oldValue = isset($this->_changes[$Model->alias]['before'][$field]) ? $this->_changes[$Model->alias]['before'][$field] : false;
+		foreach ($this->_changes[$model->alias]['after'] as $field => $newValue) {
+			$oldValue = isset($this->_changes[$model->alias]['before'][$field]) ? $this->_changes[$model->alias]['before'][$field] : false;
 			if ($newValue == $oldValue) {
 				// Values have not changed. Remove them from the arrays.
-				unset($this->_changes[$Model->alias]['before'][$field]);
-				unset($this->_changes[$Model->alias]['after'][$field]);
+				unset($this->_changes[$model->alias]['before'][$field]);
+				unset($this->_changes[$model->alias]['after'][$field]);
 			}
 		}
 
 		// Determine if any field has changed
-		$this->_changes[$Model->alias]['hasChanges'] = (boolean) $this->_changes[$Model->alias]['before'];
+		$this->_changes[$model->alias]['hasChanges'] = (boolean) $this->_changes[$model->alias]['before'];
 
 		return true;
 	}
@@ -134,29 +134,29 @@ class LogActionBehavior extends ModelBehavior {
 /**
  * After save method. Called after all saves
  *
- * @param Model $Model Model instance.
+ * @param Model $model Model instance.
  * @param boolean $created indicates whether the node just saved was created or updated
  * @return boolean true on success, false on failure
  * @access public
  */
-	public function afterSave(&$Model, $created) {
-		if ($this->_changes[$Model->alias]['hasChanges']) {
+	public function afterSave(Model $model, $created) {
+		if ($this->_changes[$model->alias]['hasChanges']) {
 			// Initial class for LogAction table
 			$logAction = ClassRegistry::init('LogAction.LogAction');
 
 
-			foreach ($this->_changes[$Model->alias]['before'] as $field => $oldValue) {
-				if (!array_key_exists($field, $this->_changes[$Model->alias]['after'])) {
+			foreach ($this->_changes[$model->alias]['before'] as $field => $oldValue) {
+				if (!array_key_exists($field, $this->_changes[$model->alias]['after'])) {
 					continue;
 				}
 
-				$newValue = $this->_changes[$Model->alias]['after'][$field];
+				$newValue = $this->_changes[$model->alias]['after'][$field];
 
 				// Insert record of this change
 				$data = array(
 					'user_id' => AuthComponent::user('id'),
-					'row'     => $Model->id,
-					'model'   => $Model->alias,
+					'row'     => $model->id,
+					'model'   => $model->alias,
 					'field'   => $field,
 					'before'  => $oldValue,
 					'after'   => $newValue
@@ -174,27 +174,27 @@ class LogActionBehavior extends ModelBehavior {
 /**
  * Called before every deletion operation.
  *
- * @param Model $Model Model instance.
+ * @param Model $model Model instance.
  * @param boolean $cascade If true records that depend on this record will also be deleted
  * @return boolean True if the operation should continue, false if it should abort
  * @access public
  */
-	public function beforeDelete(&$Model, $cascade = true) {}
+	public function beforeDelete(Model $model, $cascade = true) {}
 
 /**
  * Called after every deletion operation.
  *
- * @param Model $Model Model instance.
+ * @param Model $model Model instance.
  * @return void
  * @access public
  */
-	public function afterDelete(&$Model) {
-		if ($this->settings[$Model->alias]['trackDelete']) {
+	public function afterDelete(Model $model) {
+		if ($this->settings[$model->alias]['trackDelete']) {
 			// Insert record of this deletion
 			$data = array(
 				'user_id' => AuthComponent::user('id'),
-				'row'     => $Model->id,
-				'model'   => $Model->alias,
+				'row'     => $model->id,
+				'model'   => $model->alias,
 				'field'   => 'deleted',
 				'before'  => 0,
 				'after'   => 1
@@ -211,12 +211,12 @@ class LogActionBehavior extends ModelBehavior {
 /**
  * Allow changes to be publicly accessible
  *
- * @param Model $Model Model instance.
+ * @param Model $model Model instance.
  * @return array Changes that were made
  * @access public
  */
-	public function getChanges($Model) {
-		return $this->_changes[$Model];
+	public function getChanges($model) {
+		return $this->_changes[$model];
 	}
 
 }
